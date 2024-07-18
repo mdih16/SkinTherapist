70ad1fa4035319816874a0dde89b610a7221fd3c
mdih16 <91021533+mdih16@users.noreply.github.com>
2024-07-18 19:58:29 +0200

    modified BackButton

--- components/BackButton.js
+++ components/BackButton.js
@@ -2,11 +2,19 @@ import { TouchableOpacity, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 
-const BackButton = () => {
+const BackButton = ({ iconName, backgroundColor, onPress }) => {
   return (
-    <TouchableOpacity className="h-12 w-12">
-      <View className="flex items-center justify-center w-full h-full rounded-full bg-[#594359]/10">
-        <Ionicons name="chevron-back-outline" size={32} color="#000000" />
+    <TouchableOpacity onPress={onPress} className="h-12 w-12">
+      <View
+        className={`flex items-center justify-center w-full h-full rounded-full ${
+          !backgroundColor ? "bg-[#594359]/10" : backgroundColor
+        }`}
+      >
+        <Ionicons
+          name={!iconName ? "chevron-back-outline" : iconName}
+          size={32}
+          color="#000000"
+        />
       </View>
     </TouchableOpacity>
   );
