bc133355baf6f75ae803e893109bfe0131378741
mdih16 <91021533+mdih16@users.noreply.github.com>
2024-07-19 19:29:45 +0200

    added custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sofia: ["Sofia_400Regular"],
+        roboto_light_italic: ["Roboto_300Light_Italic"],
+        roboto_regular: ["Roboto_400Regular"],
+        roboto_medium: ["Roboto_500Medium"],
+        roboto_bold: ["Roboto_700Bold"],
+      },
+    },
   },
   plugins: [],
 };
